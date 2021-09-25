01cfb5d6f40c34852acf9e3b3000e5c058deb8f1
IvanLpCz <ivanlopezcozar@gmail.com>
2021-09-25 18:57:33 +0200

    Tuto add

--- GameJump_EsbirrosMotivados/Assets/Scripts/Buttons/PopEndPhase.cs
+++ GameJump_EsbirrosMotivados/Assets/Scripts/Buttons/PopEndPhase.cs
@@ -7,11 +7,21 @@ namespace Core
     public class PopEndPhase : MonoBehaviour
     {
         public GameObject endPhaseMenu;
+        MoveForward moveforward;
+
+        private void Start()
+        {
+            moveforward = GameObject.Find("player").GetComponent<MoveForward>();
+        }
 
         public void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Player"))
+            {
                 endPhaseMenu.SetActive(true);
+                moveforward.topSpeed = 0;
+            }
+                
         }
     }
 }
---
+++ GameJump_EsbirrosMotivados/Assets/Scripts/tuto/Tuto.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Tuto
+{
+    public class Tuto : MonoBehaviour
+    {
+        public GameObject tuto;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                tuto.SetActive(false);
+            }
+                
+        }
+    }
+}
